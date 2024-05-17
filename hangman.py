51102130d4f1267179b5d29ed116f60a0eb22183
Elena Tomeva <et@healthbiocare.at>
2024-05-17 11:14:33 +0200

    error: 'function' object has no attribute 'replace', even when status = str("_ " * n)

--- hangman.py
+++ hangman.py
@@ -11,3 +11,16 @@ def game_status(computer_choice):
     n = len(computer_choice)
     status = "_ " * n
     return status
+game_status(computer_choice)
+
+# function for player's letter input
+
+def player_letter(game_status):
+   while True:
+    user_input = str(input("Guess a letter: "))
+    if user_input in computer_choice:
+      game_status = game_status.replace("_ ", user_input)
+    else:
+      print("Sorry, this letter is not in the word. Guess another letter: ")
+
+    # AttributeError: 'function' object has no attribute 'replace', even when status = str("_ " * n)
