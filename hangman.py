9a1d6d47d7f21edaab08e9d24d2f2a11588f5522
Elena Tomeva <et@healthbiocare.at>
2024-05-31 10:40:55 +0200

    using global variable status in the function player_letter; added new_status to save the user letter input

--- hangman.py
+++ hangman.py
@@ -9,14 +9,15 @@ computer_choice = random.choice(words_options)
 # create default string with len() of the chosen random word
 def game_status(computer_choice):
     n = len(computer_choice)
-    status = "_ " * n
+    status = "_" * n
     return status
+
 status = game_status(computer_choice)
 
 # function for player's letter input
 
 
-def player_letter(game_status):
+def player_letter(status): # using the global variable status
    tries = 0
    max_tries = len(computer_choice) + 3
    while tries < max_tries:
@@ -24,15 +25,18 @@ def player_letter(game_status):
     tries += 1
     if user_input in computer_choice:
       position = computer_choice.find(user_input)
-      game_status = status[:position] + user_input + status[position + 1:]
-      print(game_status)
-      print(f"You have", max_tries - tries, "tries left.")
+      status = status[:position] + user_input + status[position + 1:] # we're already using the global variable outside the function
+      new_status = " ".join(status) # saving the user input in a new variable to which we add every new correctly guessed letter
+      print(new_status)
+      print(f"You have {max_tries - tries} tries left.")
     else:
       print("Sorry, this letter is not in the word. Guess another letter: ")
-      print(game_status)
-      print(f"You have", max_tries - tries, "tries left.")
+      print(status)
+      print(f"You have {max_tries - tries} tries left.")
     if "_" not in status:
             print("Congratulations! You've guessed the word correctly.")
             break
    else:
       print(f"Game over! The word was: {computer_choice}") 
+
+player_letter(status)
